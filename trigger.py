53d9506d89a9441490012ce900d3a4fd1886fe3a
hoppel16 <hun.oppel@gmail.com>
2020-10-30 17:51:07 -0400

    Reorganized triggers

--- trigger.py
+++ trigger.py
@@ -25,9 +25,42 @@ class Trigger1:
         lights.strobe(self.board.light1, 3, 0.05)
         self.playing = False
 
-
 class Trigger2:
-
+    def __init__(self, board):
+        self.sound = os.path.dirname(os.path.realpath(__file__)) + "/demon_trigger.mp3"
+        self.board = board
+        self.playing = False
+        sound.load_sound(self.sound)
+        
+    def play(self):
+        self.playing = True
+        sound.play_sound()
+        sleep(0.7)
+        lights.strobe(self.board.light1, 30, 0.05)
+        self.playing = False
+        
+class Trigger3:
+    def __init__(self, board):
+        self.sound = os.path.dirname(os.path.realpath(__file__)) + "/demon_trigger.mp3"
+        self.board = board
+        self.playing = False
+        sound.load_sound(self.sound)
+        
+        lights.turn_on(self.board.light2)
+        lights.turn_off(self.board.light1)
+        
+    def play(self):
+        self.playing = True
+        sound.play_sound()
+        sleep(0.7)
+        lights.turn_on(self.board.light1)
+        lights.turn_off(self.board.light2)
+        sleep(1)
+        lights.turn_off(self.board.light1)
+        lights.turn_on(self.board.light2)
+        self.playing = False
+        
+class Trigger4:
     def __init__(self, board):
         self.sound = os.path.dirname(os.path.realpath(__file__)) + "/Alma_burning_TS.mp3"
         self.board = board
@@ -43,18 +76,4 @@ class Trigger2:
         lights.strobe(self.board.light1, 7, 0.3)
         lights.strobe(self.board.light1, 5, 0.4)
         lights.strobe(self.board.light1, 3, 0.5)
-        self.playing = False
-
-class Trigger3:
-    def __init__(self, board):
-        self.sound = os.path.dirname(os.path.realpath(__file__)) + "/demon_trigger.mp3"
-        self.board = board
-        self.playing = False
-        sound.load_sound(self.sound)
-        
-    def play(self):
-        self.playing = True
-        sound.play_sound()
-        sleep(0.7)
-        lights.strobe(self.board.light1, 30, 0.05)
         self.playing = False
