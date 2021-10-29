ff5a1337b2d40f91c939a7921dc9961af094d146
Hunter Oppel <hunter.oppel@gmail.com>
2021-10-28 22:20:03 -0400

    Finished trigger5

--- trigger.py
+++ trigger.py
@@ -77,6 +77,32 @@ class Trigger4:
         self.playing = False
         
         
+class Trigger5:
+    def __init__(self, board):
+        file_name = "billy_final.mp3"
+        self.sound = os_path + file_name
+        self.board = board
+        self.playing = False
+        sound.load_sound(self.sound)
+        
+    def play(self):
+        self.playing = True
+        sound.play_sound()
+        sleep(0.45)
+        lights.strobe(self.board.light1, 9, 0.02)
+        lights.turn_off(self.board.light1)
+        sleep(0.28)
+        lights.strobe(self.board.light1, 9, 0.02)
+        lights.turn_on(self.board.light1)
+        sleep(1.95)
+        lights.strobe(self.board.light1, 2, 0.05)
+        lights.turn_off(self.board.light1)
+        sleep(0.55)
+        lights.strobe(self.board.light1, 3, 0.05)
+        lights.turn_off(self.board.light1)
+        self.playing = False
+        
+        
 class TriggerTest:
     def __init__(self, board):
         file_name = "Winnie_Glorious_Final.mp3"
