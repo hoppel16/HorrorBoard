672e7523b6eb42aea31feeb56486ab6cfbaf6ef4
Hunter Oppel <hunter.oppel@gmail.com>
2021-10-30 18:20:18 -0400

    Fixed trigger 2 and got fog machine working

--- gpio.py
+++ gpio.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 
 class Gpio:
 
-    def __init__(self, button, light1, light2=None, light3=None):
+    def __init__(self, button, light1, light2=None, light3=None, led1=None, led2=None, led3=None, led4=None, fog_on=None, fog_off=None):
         GPIO.setmode(GPIO.BCM)
         self.button = button
         setup_input(button)
@@ -11,10 +11,28 @@ class Gpio:
         setup_output(light1, "LOW")
         self.light2 = light2
         if self.light2 is not None:
-            setup_output(light2, "HIGH")
+            setup_output(light2, "LOW")
         self.light3 = light3
         if self.light3 is not None:
             setup_output(light3, "LOW")
+        self.led1 = led1
+        if self.led1 is not None:
+            setup_output(led1, "LOW")
+        self.led2 = led2
+        if self.led2 is not None:
+            setup_output(led2, "LOW")
+        self.led3 = led3
+        if self.led3 is not None:
+            setup_output(led3, "LOW")
+        self.led4 = led4
+        if self.led4 is not None:
+            setup_output(led4, "LOW")
+        self.fog_on = fog_on
+        if self.fog_on is not None:
+            setup_output(fog_on, "LOW")
+        self.fog_off = fog_off
+        if self.fog_off is not None:
+            setup_output(fog_off, "LOW")
 
 
 def setup_input(pin):
--- main.py
+++ main.py
@@ -8,13 +8,19 @@ button_pin = 16
 light1_pin = 26
 light2_pin = 6
 light3_pin = 5
-board = gpio.Gpio(button_pin, light1_pin, light2_pin, light3_pin)
+led1_pin = 17
+led2_pin = 27
+led3_pin = 22
+led4_pin = 23
+fog_on_pin = 24
+fog_off_pin = 25
+board = gpio.Gpio(button_pin, light1_pin, light2_pin, light3_pin, led1_pin, led2_pin, led3_pin, led4_pin, fog_on_pin, fog_off_pin)
 
 # Initialize audio library
 sound.init()
 
 # Choose which trigger for this board
-my_trigger = trigger.Trigger1(board)
+my_trigger = trigger.Trigger2(board)
 
 try:
     while True:
